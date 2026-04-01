1b7495aa59affcee08036f7e1bd4a641478c1a9e
kryxen <kryxen.dev@gmail.com>
2026-04-01 21:57:52 +0530

    problem 9

--- problems/9.rs
+++ problems/9.rs
@@ -1,1 +1,12 @@
-pub fn solve() {}
+pub fn solve() {
+    let max = 1000;
+    for i in 1..=max {
+        for j in i..max {
+            let k = ( i * i + j * j ) as i32;
+            let rk = k.isqrt();
+            if rk * rk == k && i + j + rk == max {
+                println!("a {} b {} c {} , {}", i, j, rk, i * j * rk);
+            }
+        }
+    }
+}
